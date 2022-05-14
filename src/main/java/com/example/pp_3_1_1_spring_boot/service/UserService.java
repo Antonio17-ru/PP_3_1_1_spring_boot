51c1b7a562aa1a4fdae84280b96f0c5d74b690e7
Antonio17-ru <AntonDemeshkin1@yandex.ru>
2022-05-14 19:44:30 +0300

    Initial commit

---
+++ src/main/java/com/example/pp_3_1_1_spring_boot/service/UserService.java
@@ -0,0 +1,35 @@
+package com.example.pp_3_1_1_spring_boot.service;
+
+import com.example.pp_3_1_1_spring_boot.repository.UserRepository;
+import com.example.pp_3_1_1_spring_boot.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(int id) {
+        return userRepository.getById(id);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(int id) {
+        userRepository.deleteById(id);
+    }
+}
